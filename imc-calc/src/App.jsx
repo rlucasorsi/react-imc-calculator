975906b9e7ca851f620cc448cce6b5c0e986cb08
Lucas Orsi <lucas.orsi@contractlabs.tech>
2026-02-18 16:16:38 -0300

    feat: add calculator reset function

--- imc-calc/src/App.jsx
+++ imc-calc/src/App.jsx
@@ -9,7 +9,33 @@ import "./App.css";
 function App() {
   const calcImc = (e, height, weight) => {
     e.preventDefault();
-    console.log("executou");
+
+    console.log(height, weight);
+
+    if (!weight || !height) return;
+
+    const weightFloat = +weight.replace(",", ".");
+    const heightFloat = +height.replace(",", ".");
+
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
+
+    setImc(imcResult);
+
+    data.forEach((item) => {
+      if (imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info);
+        setInfoClass(item.infoClass);
+      }
+    });
+
+    if (!info) return;
+  };
+
+  const resetCalc = (e) => {
+    e.preventDefault();
+    setImc("");
+    setInfo("");
+    setInfoClass("");
   };
 
   const [imc, setImc] = useState("");
@@ -18,7 +44,11 @@ function App() {
 
   return (
     <div className="container">
-      {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable />}
+      {!imc ? (
+        <ImcCalc calcImc={calcImc} />
+      ) : (
+        <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} resetCalc={resetCalc} />
+      )}
     </div>
   );
 }
