c8ab296eebb5c6f076f04d9e35362d8c6d96bcda
Lucas Orsi <lucas.orsi@contractlabs.tech>
2026-02-18 13:37:43 -0300

    feat(imc): add IMC result table component and update calculation flow

--- imc-calc/src/App.jsx
+++ imc-calc/src/App.jsx
@@ -1,11 +1,26 @@
+import { data } from "./data/data.js";
+import { useState } from "react";
+
 import ImcCalc from "./components/ImcCalc.jsx";
+import ImcTable from "./components/ImcTable.jsx";
 
 import "./App.css";
 
 function App() {
-  return <div className="container">
-    <ImcCalc/>
-  </div>;
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+    console.log("executou");
+  };
+
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
+  return (
+    <div className="container">
+      {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable />}
+    </div>
+  );
 }
 
 export default App;
